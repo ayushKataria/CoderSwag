3e30bdbd1d1d55684aa836c3718d3b20baa12ca2
ayushKataria <akat21121997Agmail.com>
2018-04-06 19:25:28 +0530

    lesson 2

--- app/src/main/java/com/example/akat2/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/akat2/coderswag/Model/Category.kt
@@ -3,4 +3,9 @@ package com.example.akat2.coderswag.Model
 /**
  * Created by Ayush Kataria on 05-04-2018.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+
+    override fun toString(): String {
+        return title
+    }
+}
