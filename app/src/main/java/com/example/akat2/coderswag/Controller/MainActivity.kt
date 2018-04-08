7de56976de37b5e08a4e35a5fcd91b55f1e05199
ayushKataria <akat21121997Agmail.com>
2018-04-08 16:48:22 +0530

    Prepare for recyclerview

--- app/src/main/java/com/example/akat2/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/akat2/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.akat2.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.akat2.coderswag.Adapters.CategoryAdapter
 import com.example.akat2.coderswag.Model.Category
 import com.example.akat2.coderswag.R
@@ -20,5 +21,7 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this,
                 DataService.categories)
         categoryListView.adapter = adapter
+
+
     }
 }
